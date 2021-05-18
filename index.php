4dea542e6e0ad4978025409f026505b7bec1b161
Muhammad Nabeel Afzal <32669244+miaannabeel@users.noreply.github.com>
2021-05-18 18:06:12 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -18,31 +18,6 @@
 		case 'instagram.com':
 			$video = new InstagramDownloader();
 			echo json_encode($video->process($url));
-			/*try {
-			    $client = new InstagramDownload($url);
-			    $url    = $client->getDownloadUrl();
-			    $type   = $client->getType();
-			    $thumb  = $client->getThumbUrl();
-			    if ($type == "image") {
-			        $type = "video/mp4";
-			    } else if ($type == "video") {
-			        $type = "video/mp4";
-			    }
-			    echo json_encode(array(
-			        "type" => $type,
-			        "thumb" => $thumb,
-			        "url" => $url,
-			        "size" => videoSize($url)
-			    ));
-			}
-			catch (\InvalidArgumentException $exception) {
-			    $error = $exception->getMessage();
-			    echo $error;
-			}
-			catch (\RuntimeException $exception) {
-			    $error = $exception->getMessage();
-			    echo $error;
-			}*/
 			break;
 		case 'twitter.com':
 			$connection = new TwitterOAuth("S6tc9bCXnA8CeGy8wOBTu98LF", "9O7YNsviXvjZdrVbj0sGjTrIUfv5L5hdGDi1D1UoKylgcv3gDT", "204253793-L80p6Tu1mR6AmgzP1GwWUPtBdJpDF8dafiEaDRE4", "XzRcrCbqM3G2DQyWkOmBgANhMNBlIsRaejfy6k8RNqTwC");
